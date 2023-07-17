d86ff2b63ada5fb02d0f437bf781af53ef382223
new name <vov4ik.samurai@yandex.ru>
2023-07-17 12:45:19 +0300

    added folder component header + UI component google button

---
+++ components/Header.tsx
@@ -0,0 +1,49 @@
+"use client";
+import {type FC, useMemo} from 'react';
+import {usePathname, useRouter} from "next/navigation";
+import Link from "next/link";
+import {useSession, signIn, signOut} from "next-auth/react";
+import Image from "next/image";
+
+type NavLink = {
+    label: string;
+    active: boolean;
+    href: string;
+}
+
+const Header: FC = () => {
+    const pathname: string = usePathname();
+    const session = useSession();
+    const router = useRouter();
+    const routes: NavLink[] = useMemo(() => [
+        {
+            label: 'Home',
+            active: pathname === '/',
+            href: '/',
+        },
+    ], [pathname]);
+
+    return (
+        <header className="flex items-center justify-center gap-x-8 px-4 py-2 bg-gray-500">
+
+            <nav>
+                <ul className="flex items-center justify-center gap-x-6">
+                    {routes.map(link => (
+                        <li key={link.label}><Link className={link.active ? 'text-white' : 'text-neutral-400'} href={link.href}>{link.label}</Link></li>
+                    ))}
+                </ul>
+            </nav>
+
+            {session?.data ? <Link href="#" onClick={() => signOut({
+                callbackUrl: '/'})}>Sing Out</Link> : <Link href="/api/auth/signin">Sing In</Link>}
+
+            {session?.data &&
+                <Image priority src={session?.data?.user?.image!} onClick={() => router.push('/profile')}
+                       className={`rounded-full cursor-pointer ${pathname === '/profile' && "border-2 border-b-amber-200"}`} width={25} height={25}
+                       alt="Profile Image"/>}
+
+        </header>
+    );
+};
+
+export default Header;
---
+++ components/UI/GoogleButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+import {type FC} from 'react';
+import {signIn} from "next-auth/react";
+import {useSearchParams} from "next/navigation";
+
+const GoogleButton: FC = () => {
+    const callbackUrl: string = useSearchParams().get('callbackUrl') || '/profile';
+
+    return (
+        <button className="rounded-xl bg-blue-500 p-2.5 text-white" onClick={() => signIn('google', {callbackUrl})}>
+            Sign in with Google
+        </button>
+    );
+};
+
+export default GoogleButton;
